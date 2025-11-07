18cae055c1d0e2212a877f3d6be47798244ddb7a
Tamara <135102183+Tamarasza999@users.noreply.github.com>
2025-11-07 11:59:16 +1100

    Updated

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
-  # Force path-style S3 URLs for LocalStack
+  #force path style S3 URLs for localstack
   s3_use_path_style           = true
   
   endpoints {
