ff7bf962f251f4a765a91933a4aa18984f177934
Tamara <135102183+Tamarasza999@users.noreply.github.com>
2025-11-07 12:00:19 +1100

    Updated

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "app_data" {
   bucket = "${var.env}-app-data-bucket"
   tags = { Environment = var.env }
   
-  # Force path-style addressing for LocalStack
+  
   force_destroy = true
 }
 
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "logs" {
   bucket = "${var.env}-logs-bucket" 
   tags = { Environment = var.env }
   
-  # Force path-style addressing for LocalStack
+  
   force_destroy = true
 }
 
